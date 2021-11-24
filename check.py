a7c34ae453e7dfe14ba91631edbb401dbd88782c
Liron <lironpaz7@gmail.com>
2021-11-24 19:45:13 +0200

    general fixes

--- check.py
+++ check.py
@@ -1,4 +1,7 @@
 import itertools
+import math
+
+import networkx as nx
 
 import ex1
 import search
@@ -343,7 +346,7 @@ def main():
 
     ]
     print('problems:')
-    solve_problems(problems)
+    solve_problems(problems[3:])
     print('non_competitive_inputs:')
     solve_problems(non_competitive_inputs)
     print('competitive_inputs:')
@@ -352,3 +355,52 @@ def main():
 
 if __name__ == '__main__':
     main()
+import pprint
+import random
+from random import randrange
+import string
+
+
+def create_test(max_length, max_width, drone_amount, pckg_amount, client_amount, seed=None):
+    """
+    Generates random tests for drone assignment.
+    recommended use is like this: "problems = [create_test(5, 5, 2, 4, 2) for i in range(number_of_wanted_tests)]
+
+    :param max_length: maximum length of map, at least 3
+    :param max_width:  maximum width of map, at least 3
+    :param drone_amount: maximum drone amount on map
+    :param pckg_amount: maximum package amount on map
+    :param client_amount: maximum client amount on map
+    :param seed: (optional) seed to get pseudorandom numbers with fixed values over many iterations.
+    :return: a single test
+    """
+    assert max_width > 4
+    assert max_length > 4
+    if seed is not None:
+        random.seed(seed)
+    length = randrange(4, max_length)
+    width = randrange(4, max_width)
+    test = {"map": [['P' if random.random() > 0.15 else 'I' for i in range(length)] for j in range(width)],
+            "drones": {f'drone {i}': (randrange(1, width), randrange(1, length)) for i in
+                       range(randrange(1, drone_amount + 1))},
+            "packages": {f'package {i}': (randrange(1, width), randrange(1, length)) for i in
+                         range(randrange(1, pckg_amount + 1))},
+            "clients":
+                {''.join(random.choices(string.ascii_uppercase, k=5)):
+                     {"path": [(randrange(1, width), randrange(1, length)) for i in range(randrange(1, 6))],
+                      "packages": []} for i in range(randrange(1, client_amount + 1))}}
+    # assign package to random client
+
+    for pckg, _ in test['packages'].items():
+        random_client = random.choice(list(test['clients'].keys()))
+        test['clients'][random_client]['packages'].append(pckg, )
+
+    for name, data in test['clients'].items():
+        data['packages'] = (*data['packages'],)
+
+    return test
+
+# if __name__ == '__main__':
+#     tests = [create_test(5, 5, 2, 4, 2) for i in range(5)]
+#     solve_problems(tests)
+#     pprint.pprint(tests)
--- ex1.py
+++ ex1.py
@@ -6,6 +6,7 @@ import random
 import copy
 import json
 import math
+import networkx as nx
 
 import utils
 
@@ -14,13 +15,6 @@ ids = ["311280283", "313535379"]
 
 def manhattan(a, b):
     return sum(abs(val1 - val2) for val1, val2 in zip(a, b))
-
-
-def manhattan_pro(game_map, a, b):
-    if a == b:
-        return 1
-    if 1:
-        pass
 
 
 def euclidean(a, b):
@@ -38,17 +32,28 @@ class DroneProblem(search.Problem):
         drones = initial['drones']
         packages = initial['packages']
         # updates the map
-        self._where_I = self._update_map(game_map, packages)
+        self._update_map(game_map, packages)
+        G = self._build_graph(game_map)
+        self._short_distances = self._create_shortest_path_distances(G)
         clients = initial['clients']
         # goal
         client_packages_counter = self.create_packages_counter(clients)
         turns = 0
         # checks if there is no solution
         # self._are_all_packages_reachable(clients, game_map, packages)
-        coordinate_to_packages = self.get_packages_at_coordinate(packages)
+        # creates the coordinate to package dictionary with relevant packages only
+        relevant_packages = set()
+        for client in clients.values():
+            for package in client['packages']:
+                relevant_packages.add(package)
+        # updates packages with relevant packages only
+        for package in set(packages.keys()).difference(relevant_packages):
+            packages.pop(package)
+        coordinate_to_packages = self.get_packages_at_coordinate(packages, relevant_packages)
         # -------- drones data --------
         # drones packages
         drones_packages = {name: [] for name in drones.keys()}
+        self._package_name_to_client_name = self._create_package_name_to_client_name(clients)
         # set state as a tuple of wanted features
         state = {'clients': clients,
                  'game_map': game_map,
@@ -59,8 +64,6 @@ class DroneProblem(search.Problem):
                  'drones_packages': drones_packages,
                  'turns': turns}
         state = json.dumps(state)
-        # saves the initial state for planning better heuristic later
-        self.root = state
         search.Problem.__init__(self, state)
 
     def actions(self, state):
@@ -98,7 +101,6 @@ class DroneProblem(search.Problem):
             state['drones'][drone_name] = (x_new, y_new)
         # action = pickup --> ('pick up', drone_name, package_name)
         elif drone_action[0] == 'pick up':
-            # print(drone_action, state)
             drone_name = drone_action[1]
             package_name = drone_action[2]
             # extract package coordinates
@@ -125,16 +127,16 @@ class DroneProblem(search.Problem):
         """ Given a state, checks if this is the goal state.
          Returns True if it is, False otherwise."""
         state = json.loads(state)
-        return sum([num for num in state['client_packages_counter'].values()]) == 0
+        return sum([packages_left for packages_left in state['client_packages_counter'].values()]) == 0
 
     def h(self, node):
         """ This is the heuristic. It gets a node (not a state,
         state can be accessed via node.state)
         and returns a goal distance estimate"""
-        state = json.loads(node.state)
+        # state = json.loads(node.state)
         # print(state['turns'], state['drones_packages'], state['drones'], node.action)
         # return self._h2(node)
-        return self._h1_copy(node)
+        return self._h1_copy3(node)
 
     def _h1(self, node):
         """
@@ -347,16 +349,89 @@ class DroneProblem(search.Problem):
                 distances_manhattan += manhattan(drone_loc, package_loc)
                 # euclidean distances
                 distances_euclidean += euclidean(drone_loc, package_loc)
-                # for I_loc in self._where_I:
-                #     distances_to_I += manhattan(drone_loc, I_loc)
                 if distances_manhattan == 0:
                     # bonus for intersection with client
                     distances_manhattan -= 1
                     distances_euclidean -= 1
 
-        penalty = max(distances_manhattan,
-                      distances_euclidean) + (packages_on_drones * 3) + (
-                          packages_on_board * 8) + distances_to_clients + circles + node.path_cost
+        penalty = max(distances_euclidean, distances_manhattan) + (packages_on_drones * 3) + (
+                packages_on_board * 8) + distances_to_clients + circles + node.path_cost
+        # print(penalty)
+        return penalty
+
+    def _h1_copy3(self, node):
+        """
+        Best h so far
+        :param node:
+        :return:
+        """
+        state = json.loads(node.state)
+        circles = 0
+        if node.action is not None:
+            if type(node.action[0]) != tuple:
+                if node.action[0] == 'deliver':
+                    # deliver is the best choice always!!! we are not stupid
+                    return -10
+                elif node.action[0] == 'move':
+                    # check if we are in a cycle --> penalty of 10
+                    parent = node.parent
+                    if parent is not None:
+                        parent = parent.parent
+                        if parent is not None:
+                            parent_state = json.loads(parent.state)
+                            for drone_name, drone_loc in parent_state['drones'].items():
+                                current_turn_drone_name, current_turn_drone_move = drone_name, tuple(
+                                    state['drones'][drone_name])
+                                if tuple(drone_loc) == current_turn_drone_move:
+                                    circles += 100
+            else:
+                for act in node.action:
+                    if act[0] == 'deliver':
+                        return -10
+                    elif node.action[0] == 'move':
+                        # implement cycle check for multiple drones
+                        # check if we are in a cycle --> penalty of 10
+                        parent = node.parent
+                        if parent is not None:
+                            parent = parent.parent
+                            if parent is not None:
+                                parent_state = json.loads(parent.state)
+                                current_turn_drone_name = node.action[1]
+                                current_turn_drone_move = tuple(state['drones'][current_turn_drone_name])
+                                parent_drone_loc = tuple(parent_state['drones'][current_turn_drone_name])
+                                if parent_drone_loc == current_turn_drone_move:
+                                    circles += 100
+
+        packages_on_board = len(state['packages'].keys())
+        packages_on_drones = sum([len(p) for p in state['drones_packages']])
+        distances_manhattan, distances_euclidean, distances_to_clients = 0, 0, 0
+
+        if packages_on_drones > 0:
+            # dist to client
+            for drone_name, drone_loc in state['drones'].items():
+                for package_name in state['drones_packages'][drone_name]:
+                    client_name = self._package_name_to_client_name[package_name]
+                    path = state['clients'][client_name]['path']
+                    # min_dist_to_client_path = min([manhattan(drone_loc, path[i]) for i in range(len(path))])
+                    client_loc_next_turn = utils.turn_heading(path[0], state['turns'] + 1, path)
+                    distances_to_clients += manhattan(tuple(drone_loc), tuple(client_loc_next_turn))
+                    # key = (tuple(drone_loc), tuple(client_loc_next_turn))
+                    # if key in self._short_distances:
+                    #     distances_to_clients += self._short_distances[key]
+
+        for drone_loc in state['drones'].values():
+            for package_loc in state['packages'].values():
+                # manhattan distances
+                key = (tuple(drone_loc), tuple(package_loc))
+                if key in self._short_distances:
+                    distances_manhattan += self._short_distances[key]
+                # distances_euclidean += euclidean(drone_loc, package_loc)
+                if distances_manhattan == 0:
+                    # bonus for intersection with client
+                    distances_manhattan -= 1
+
+        penalty = distances_manhattan + (packages_on_drones * 3) + (
+                packages_on_board * 8) + distances_to_clients + circles + node.path_cost
         # print(penalty)
         return penalty
 
@@ -396,7 +471,7 @@ class DroneProblem(search.Problem):
         # print(distances)
         return distances
 
-    def create_package_name_to_client_name(self, clients):
+    def _create_package_name_to_client_name(self, clients):
         """
         Creates a dictionary where package_name is a key and client name is value
         :return: dictionary
@@ -412,16 +487,13 @@ class DroneProblem(search.Problem):
         """
         Updates the map with the given packages, increment the amount of packages in the specific cell.
         """
-        where_I = set()
         for x, y in packages.values():
             if game_map[x][y] == 'I':
-                where_I.add((x, y))
                 continue
             if game_map[x][y] == 'P':
                 game_map[x][y] = 1
             else:
                 game_map[x][y] += 1
-        return where_I
 
     def drone_can_pick_up_package(self, drones_packages, drone_name):
         """
@@ -431,7 +503,7 @@ class DroneProblem(search.Problem):
         """
         return len(drones_packages[drone_name]) < 2
 
-    def drone_can_deliver_package(self, clients, package_name, x, y, turns):
+    def who_can_receive_package(self, clients, package_name, x, y, turns):
         """
         Checks if there is a client at position (x,y) that would like to receive 'pack_name'
         :param x: pos x
@@ -439,7 +511,7 @@ class DroneProblem(search.Problem):
         :param package_name: package name
         :return: client name or None if doesn't exist
         """
-        client_name = self.create_package_name_to_client_name(clients)[package_name]
+        client_name = self._package_name_to_client_name[package_name]
         client_movements = clients[client_name]['path']
         client_pos = utils.turn_heading(client_movements[0], turns, client_movements)
         return client_name if (x, y) == tuple(client_pos) else None
@@ -484,7 +556,7 @@ class DroneProblem(search.Problem):
 
             for pack_name in state['drones_packages'][drone_name]:
                 # check all packages for this specific drone
-                client_name = self.drone_can_deliver_package(state['clients'], pack_name, x, y, state['turns'])
+                client_name = self.who_can_receive_package(state['clients'], pack_name, x, y, state['turns'])
                 if client_name is not None:
                     # there is a client that can receive the 'pack_name'
                     # add action
@@ -555,14 +627,17 @@ class DroneProblem(search.Problem):
             d[client] = len(clients[client]['packages'])
         return d
 
-    def get_packages_at_coordinate(self, packages):
+    def get_packages_at_coordinate(self, packages, relevant_packages):
         """
         Creates a dictionary with keys as coordinates and values as package name
-        :param packages:
+        :param relevant_packages: relevant game packages
+        :param packages: game packages
         :return:
         """
         position_to_packages = {}
         for package_name, coordinate in packages.items():
+            if package_name not in relevant_packages:
+                continue
             coordinate = str(tuple(coordinate))
             if coordinate in position_to_packages:
                 position_to_packages[coordinate].append(package_name)
@@ -570,8 +645,32 @@ class DroneProblem(search.Problem):
                 position_to_packages[coordinate] = [package_name]
         return position_to_packages
 
-    def tuplify(self, d: dict) -> tuple:
-        return tuple([(k, v) for k, v in d.items()])
+    def _build_graph(self, game_map):
+        G = nx.Graph()
+        rows, cols = len(game_map), len(game_map[0])
+        for i in range(rows):
+            for j in range(cols):
+                if game_map[i][j] == 'I':
+                    continue
+                # edge from (i,j) to its adjacent: (i+1,j), (i-1,j), (i,j+1), (i,j-1)
+                if i + 1 < rows and game_map[i + 1][j] != 'I':
+                    G.add_edge((i, j), (i + 1, j))
+                if i - 1 >= 0 and game_map[i - 1][j] != 'I':
+                    G.add_edge((i, j), (i - 1, j))
+                if j + 1 < cols and game_map[i][j + 1] != 'I':
+                    G.add_edge((i, j), (i, j + 1))
+                if j - 1 >= 0 and game_map[i][j - 1] != 'I':
+                    G.add_edge((i, j), (i, j - 1))
+        return G
+
+    def _create_shortest_path_distances(self, G):
+        d = {}
+        for n1 in G.nodes:
+            for n2 in G.nodes:
+                if n1 == n2:
+                    continue
+                d[(n1, n2)] = len(nx.shortest_path(G, n1, n2)) - 1
+        return d
 
 
 def create_drone_problem(game):
